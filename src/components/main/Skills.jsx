a9cfc5230e5bec561a67b61739164a793104760e
Facundo <facundotisserasorribas@gmail.com>
2022-08-19 21:34:01 -0300

    creacion de skills con slider

--- src/components/main/Skills.jsx
+++ src/components/main/Skills.jsx
@@ -8,68 +8,53 @@ import shoppingCart from '../../assets/image/shoppingCart.png';
 
 
 function Skills() {
-    const images = [maintenance, working, shoppingCart];
+    const imagenes = [
+        {
+            id: 1,
+            image: working,
+            title: 'Mantenimiento',
+            parrafo: 'Mantenimiento y actualizaciones de páginas web ya creadas.',
+        },
+        {
+            id: 2,
+            image: maintenance,
+            title: 'Desarrollo-Web',
+            parrafo: ' Realizamos desarrollo web a medida, altamente optimizado. Web estructuradas y web autodministrables.',
+        },
+        {
+            id: 3,
+            image: shoppingCart,
+            title: 'E-commerce',
+            parrafo: 'Creamos tu tienda online para potenciar tus ventas.',
+        }
+    ];
+
     return (
         <div>
             <h2 className='color'>¡Un subtitulo para aquí!</h2>
 
-                <p>Aprovecha esta sección para resaltar los beneficios de tu producto o servicio. Las palabras clave ayudan con el SEO.</p>            
+            <p>Aprovecha esta sección para resaltar los beneficios de tu producto o servicio. Las palabras clave ayudan con el SEO.</p>
 
-                <div>
-                    <motion.div className='slider-container'>
-                        <motion.div className='slider' drag='x'
-                         dragConstraints= {{right:153, left: -480}}>
-                            <div>
-                                <motion.div className='item'>
-                                    <div className='container-img'>
-                                        <img src={working} alt="" />
-                                    </div>
+            <motion.div className='slider-container'>
+                <motion.div className='slider' drag='x'
+                    dragConstraints={{ right: 23, left: -718 }}>
 
-                                    <div className='container-title-description'>
-                                        <h3>Mantenimiento</h3>
-                                        <p>
-                                            Mantenimiento y actualizaciones de páginas web ya creadas.
-                                        </p>
-                                    </div>
-                                </motion.div>
-                            </div>
-                            <div>
-                                <motion.div className='item'>
-                                    <div className='container-img'>
-                                        <img src={maintenance} alt="" />
-                                    </div>
+                    {
+                        imagenes.map((imagen, i) => (
+                            <motion.div key={imagen.id} className='item'>
+                                <img src={imagen.image} alt={imagen.title} className='container-img' />
+                                <div className='conteiner-title-description'>
+                                    <h3>{imagen.title}</h3>
+                                    <p className='parrafo-skills'>{imagen.parrafo}</p>
+                                </div>
+                            </motion.div>
+                        ))
+                    }
+                </motion.div>
+            </motion.div>
 
-                                    <div className='container-title-description'>
-                                        <h3>Desarrollo Web</h3>
-                                        <p>
-                                            Realizamos desarrollo web a medida, altamente optimizado. Web estructuradas y web autodministrables.
-                                        </p>
-                                    </div>
-                                </motion.div>
-                            </div>
-                            
-                            <div>
-                                <motion.div className='item'>
-                                    <div className='container-img'>
-                                        <img src={shoppingCart} alt="" />
-                                    </div>
-
-                                    <div className='container-title-description'>
-                                        <h3>E-commerce</h3>
-                                        <p>
-                                            Creamos tu tienda online para potenciar tus ventas.
-                                        </p>
-                                    </div>
-                                </motion.div>
-                            </div>
-                        </motion.div>
-                    </motion.div>
-                
-                </div>
-                    
-        
         </div>
     )
-    }
+}
 
 export default Skills
