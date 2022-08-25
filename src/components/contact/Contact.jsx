50238ed74fd9bb266070b05142301aa871ee88f2
Javier Vargas <jabo94454@gmail.com>
2022-08-24 20:26:11 -0500

    se realizan cambios agregados

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,62 @@
 import chat from "./../../assets/image/chat.png";
 import chat2 from "./../../assets/image/chatDesktop.png";
 import whatsapp from "./../../assets/image/whatsapp.png";
+import email from "./../../assets/image/icons/email.png";
+import phone from "./../../assets/image/icons/phone.png";
+import facebook from "./../../assets/image/icons/facebook.png";
+import instagram from "./../../assets/image/icons/instagram.png";
 
 const Contact = () => {
   return (
     <section className='container__contact'>
-      <img className='image' src={chat} alt='Imagen para contactos' />
-      <img className='desktop' src={chat2} alt='Imagen para contactos' />
-      <h2 className='subtitle'>¿Aún tienes dudas? ¡Contáctame!</h2>
-      <h2 className='subtitle hidden'>Hablemos de tu proyecto</h2>
-      <div className='content__decoration'>
-        <div className='decoration'></div>
-      </div>
       <section className='container__contact--desktop'>
         <section className='content__contact--desktop'>
-          <p className='parraph'>
-            Ofrecer un medio directo de comunicación. Puede ser un
-            contacto@maxllallire.com.ar ¡O cree un formulario al costado para
-            ponerse en contacto lo antes posible!
-          </p>
-          <article className='container__whatsapp'>
-            <img src={whatsapp} alt='Ícono de Whatsapp' />
-            <p>¡Oye! ¡Estamos en whatsapp!</p>
-          </article>
+          <section className='content__contact--text'>
+            <img className='image' src={chat} alt='Imagen para contactos' />
+            <h2 className='subtitle'>¿Aún tienes dudas? ¡Contáctame!</h2>
+            <section className='section__contact--text-desktop'>
+              <p className='text__red desktop'>CONTACTAME</p>
+              <h2 className='subtitle hidden'>Hablemos sobre tu proyecto</h2>
+              <div className='content__decoration'>
+                <div className='decoration'></div>
+              </div>
+            </section>
+            <p className='parraph image'>
+              Ofrecer un medio directo de comunicación. Puede ser un
+              contacto@maxllallire.com.ar ¡O cree un formulario al costado para
+              ponerse en contacto lo antes posible!
+            </p>
+            <article className='container__whatsapp'>
+              <article className='content__social'>
+                <img className='image' src={whatsapp} alt='Ícono de Whatsapp' />
+                <p className='image'>¡Oye! ¡Estamos en whatsapp!</p>
+              </article>
+              <article className='content__social'>
+                <img className='desktop' src={email} alt='Ícono de Whatsapp' />
+                <p className='desktop'>maximollallire@gmail.com</p>
+              </article>
+              <article className='content__social'>
+                <img className='desktop' src={phone} alt='Ícono de Whatsapp' />
+                <p className='desktop'>1123921321</p>
+              </article>
+            </article>
+          </section>
+          <section className='content__contact--social desktop'>
+            <p>Sigamos en contacto </p>
+            <section>
+              <a className='contact__a' href=''>
+                <img src={instagram} alt='Ícono de Instagram' />
+              </a>
+              <a className='contact__a' href=''>
+                <img src={facebook} alt='Ícono de Facebook' />
+              </a>
+            </section>
+          </section>
         </section>
         <form className='form__contact' action=''>
+          <div className='form__container--image'>
+            <img className='desktop' src={chat2} alt='Imagen para contactos' />
+          </div>
           <div>
             <label htmlFor=''>Name</label>
             <input type='text' placeholder='Nombre' />
@@ -36,6 +68,10 @@ const Contact = () => {
           <div>
             <label htmlFor=''>Telefone</label>
             <input type='number' placeholder='+51 11 9999 9999' />
+          </div>
+          <div>
+            <label htmlFor=''>Mensaje</label>
+            <input type='text' placeholder='Mensaje' />
           </div>
           <button>Enviar</button>
         </form>
--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,17 +1,88 @@
 import esfera from "../../assets/image/esfera.png";
-import man from "./../../assets/image/man.png";
+import man from "../../assets/image/man.png";
+import html from "../../assets/image/lenguages/html.png";
+import bootstrap from "../../assets/image/lenguages/bootstrap.png";
+import css from "../../assets/image/lenguages/css.png";
+import firebase from "../../assets/image/lenguages/firebase.png";
+import javascript from "../../assets/image/lenguages/javascript.png";
+import jquery from "../../assets/image/lenguages/jquery.png";
+import react from "../../assets/image/lenguages/react.png";
+import sass from "../../assets/image/lenguages/sass.png";
+import git from "../../assets/image/lenguages/git.png";
 
 const Service = () => {
   return (
     <section className='section__service'>
       <section className='section__text'>
         <p className='hashtag'>#MAXLLALLIRE</p>
-        <h2 className='title'>Sencillo, rápido y a tu manera.</h2>
-        <p className='parragraph'>
+        <h2 className='title mobile'>Sencillo, rápido y a tu manera</h2>
+        <h2 className='title desktop'>Habilidades</h2>
+        <p className='parragraph desktop'>
+          Mis aptitudes y validaciones destacadas.
+        </p>
+        <p className='mobile'>
           Muestra los valores de tu marca y cómo puede ser parte de la vida del
           usuario que está leyendo. Generar identificación es la clave para
           conquistar a tu cliente.
         </p>
+        <section className='section__lenguages desktop'>
+          <section className='section__first'>
+            <article className='article__lenguage'>
+              <img src={html} alt='Ícono de HTML' />
+              <p>HTML</p>
+            </article>
+            <article className='article__lenguage'>
+              <img src={css} alt='Ícono de CSS' title='Ícono de CSS' />
+              <p>CSS</p>
+            </article>
+            <article className='article__lenguage'>
+              <img src={sass} alt='Ícono de SASS' title='Ícono de SASS' />
+              <p>SASS</p>
+            </article>
+            <article className='article__lenguage'>
+              <img
+                src={bootstrap}
+                alt='Ícono de BOOTSTRAP'
+                title='Ícono de BOOTSTRAP'
+              />
+              <p>BOOTSTRAP</p>
+            </article>
+            <article className='article__lenguage'>
+              <img src={git} alt='Ícono de GIT' title='Ícono de GIT' />
+              <p>GIT</p>
+            </article>
+          </section>
+          <section className='section__second'>
+            <article className='article__lenguage'>
+              <img
+                src={javascript}
+                alt='Ícono de JAVASCRIPT'
+                title='Ícono de JAVASCRIPT'
+              />
+              <p>JAVASCRIPT</p>
+            </article>
+            <article className='article__lenguage'>
+              <img src={jquery} alt='Ícono de JQUERY' title='Ícono de JQUERY' />
+              <p>JQUERY</p>
+            </article>
+            <article className='article__lenguage'>
+              <img
+                src={react}
+                alt='Ícono de REACT JS'
+                title='Ícono de REACT JS'
+              />
+              <p>REACT JS</p>
+            </article>
+            <article className='article__lenguage'>
+              <img
+                src={firebase}
+                alt='Ícono de FIREBASE'
+                title='Ícono de FIREBASE'
+              />
+              <p>FIREBASE</p>
+            </article>
+          </section>
+        </section>
       </section>
       <section className='container__design'>
         <div className='presentation-color' />
