f629c79d6d31548adbe8a4901cfb9997a5063c01
Javier Vargas <jabo94454@gmail.com>
2022-08-31 08:29:56 -0500

    [FIX] correccion de importacion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import "../assets/scss/base/App.scss";
 
 // COMPONENTS
 import Presentation from "./header/presentation/Presentation";
-import Skills from "./main/skills";
+import Skills from "./main/Skills";
 import Contact from "./contact/Contact";
 import Footer from "./footer/Footer";
 import Service from "./service/Service";
